3a07bb7d68582e3f9e5f75a384f8d90a6d0b4cda
kontrollanten <alexander@width.se>
2019-03-13 23:50:19 +0100

    feat: add colors for more mimes

--- src/lib/get-style-for-entry.js
+++ src/lib/get-style-for-entry.js
@@ -1,16 +1,21 @@
 const backgroundColors = {
   "text/html other": "blue",
+  "text/html script": "blue",
   "text/css parser": "aqua",
   "application/javascript parser": "teal",
   "image/svg+xml parser": "olive",
   "application/octet-stream parser": "green",
+  "application/octet-stream script": "lightblue",
   "application/javascript script": "lime",
   "text/javascript script": "yellow",
   "application/json script": "orange",
+  "application/json other": "mediumorchid",
   "image/gif script": "red",
+  "image/png other": "mediumpurple",
   "text/plain script": "fuchsia",
   "image/x-icon other": "purple",
-  "text/css script": "maroon"
+  "text/css script": "maroon",
+  "x-unknown parser": "moccasin",
 };
 
 const textColors = {
