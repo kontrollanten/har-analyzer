a66cd43140109fc8e2f1829544e836303846d7e9
kontrollanten <alexander@width.se>
2019-03-17 23:39:54 +0100

    refactor(MimeTimeline): move styling to scss

--- src/presentation/MimeTimeline.js
+++ src/presentation/MimeTimeline.js
@@ -1,20 +1,18 @@
 import { h } from 'preact';
 import getStyleForEntry from '../lib/get-style-for-entry';
 import formatSeconds from '../lib/format-seconds';
+import styles from './mime-timeline.scss';
 
 const MimeTimeline = ({ contentLoadTime, endTime, loadTime, mimeEntries, startTime }) => {
   const totalDuration = endTime - startTime;
   const stepSize = (totalDuration/5);
 
   return (
-    <div style={{ position: 'relative', width: '100%' }}>
+    <div className={styles.MimeTimeline}>
       <h2>MIME timeline</h2>
 
       <div
-        style={{
-          height: '20px',
-          position: 'relative',
-        }}
+        className={styles.XAxesLabel}
       >
       {Array.from(new Array(Math.round(5)))
           .map((_b, i) => Math.round((i * stepSize)))
@@ -22,22 +20,15 @@ const MimeTimeline = ({ contentLoadTime, endTime, loadTime, mimeEntries, startTi
             <div
               style={{
                 left: `${n*100/totalDuration}%`,
-                position: 'absolute',
               }}
             >{n/1000} s</div>
           ))}
       </div>
 
-      <div style={{
-        position: 'relative',
-      }}>
+      <div className={styles.Content}>
         <div
+          className={styles.OnLoadMarker}
           style={{
-            position: "absolute",
-            width: "3px",
-            backgroundColor: "green",
-            top: 0,
-            bottom: 0,
             left:
               (loadTime / totalDuration) *
                 100 +
@@ -47,12 +38,8 @@ const MimeTimeline = ({ contentLoadTime, endTime, loadTime, mimeEntries, startTi
         />
 
         <div
+          className={styles.ContentLoadMarker}
           style={{
-            position: "absolute",
-            width: "3px",
-            backgroundColor: "red",
-            top: 0,
-            bottom: 0,
             left:
               (contentLoadTime / totalDuration) *
                 100 +
@@ -64,10 +51,7 @@ const MimeTimeline = ({ contentLoadTime, endTime, loadTime, mimeEntries, startTi
         {mimeEntries
             .map(entry => (
               <div
-                style={{
-                  height: 20,
-                  position: 'relative',
-                }}
+                className={styles.MimeRow}
               >
                 {entry.timings
                 .map(entry => ({
@@ -77,12 +61,10 @@ const MimeTimeline = ({ contentLoadTime, endTime, loadTime, mimeEntries, startTi
                 }))
                 .map((time, index) => (
                   <div
+                    className={styles.MimeTimeSpan}
                     style={{
                       ...getStyleForEntry({ ...entry, initiator: entry.initiatorType }),
                       left: `${(time.relStartTime)*100/totalDuration}%`,
-                      height: '20px',
-                      position: 'absolute',
-                      whiteSpace: 'nowrap',
                       width: `${(time.relEndTime-time.relStartTime)*100/totalDuration}%`,
                     }}
                     title={`${formatSeconds(time.relStartTime)} - ${formatSeconds(time.relEndTime)}\n${time.urls.length} requests:\n${time.urls.join('\n')}`}
